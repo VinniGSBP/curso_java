54f54a3d3c83a8f9058f08f6ba6344e6342f5ee5
VinniGSBP <gs.vinni@gmail.com>
2025-11-15 21:22:35 -0300

    Exemplo aula Herdar vs cumprir contrato

---
+++ herdar_vs_cumprirContrato/src/appliaction/Program.java
@@ -0,0 +1,22 @@
+package appliaction;
+
+
+import java.util.Locale;
+
+import entities.enums.Color;
+import model.entities.AbstractShape;
+import model.entities.Circle;
+import model.entities.Rectangle;
+
+public class Program {
+	public static void main(String[]args) {
+	Locale.setDefault(Locale.US);
+	AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+	AbstractShape s2 = new Rectangle(Color.BLUE, 3.0, 4.0);
+	
+	System.out.println("Circle color: " + s1.getColor());
+	System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+	System.out.println("Rectangle color: " + s2.getColor());
+	System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+	}
+}
